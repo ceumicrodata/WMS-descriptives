5f516f8d531bf3a48c6ef716eb114a94d6a58670
Gergely Attila Kiss <corra971407@gmail.com>
2024-08-12 18:04:19 +0200

    update balance.jl to work with up-to-date Kezdi

--- src/read/balance.jl
+++ src/read/balance.jl
@@ -1,5 +1,4 @@
 using Kezdi
-using CSV
 
 left(text::AbstractString, n::Int) = n > 0 ? text[1:min(n, end)] : text[1:end-min(-n, end)]
 right(text::AbstractString, n::Int) = n > 0 ? text[end-min(n, end)+1:end] : text[min(-n, end)+1:end]
@@ -11,16 +10,16 @@ setdf(df)
 
 @keep @if year <= 2018 && year >= 2016
 @keep frame_id originalid year county fo3 teaor_raw teaor03_2d teaor03_1d teaor08_2d teaor08_1d foundyear sales_clean emp tanass_clean Export ranyag wbill immat final_netgep
-
 @generate frame_id_numeric = parse(Int64, right(frame_id, -2)) @if left(frame_id, 2) == "ft"
-@keep @if !ismissing(frame_id_numeric) && !ismissing(originalid)
+@keep @if !ismissing(originalid) & !ismissing(frame_id_numeric)
 @rename originalid tax_id
 
-@egen last_year = maximum(cond(!(ismissing(sales_clean) & ismissing(emp)), year, 0)), by(tax_id)
+@generate cond = cond(!ismissing(sales_clean) && !ismissing(emp), year, -99)
+@egen last_year = maximum(cond), by(tax_id)
 @tabulate last_year
 @keep @if last_year == year
-@drop last_year aux_cond
+@drop last_year cond
 
-@mvencode sales_clean emp tanass_clean Export ranyag wbill immat final_netgep county teaor_raw teaor03_2d teaor_08_2d, mv(0)
+@mvencode sales_clean emp tanass_clean Export ranyag wbill immat final_netgep county teaor_raw teaor03_2d teaor08_2d, mv(0)
 
-CSV.write("temp/balance.csv", df)
+@save "temp/balance.dta", replace
