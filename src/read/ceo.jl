14c35aafc861d4cc730674a10e82f511312a6729
Gergely Attila Kiss <corra971407@gmail.com>
2024-08-13 16:35:49 +0200

    working but not matching version of ceo.jl

--- src/read/ceo.jl
+++ src/read/ceo.jl
@@ -5,39 +5,41 @@ using Kezdi
 # propagate birth year to all years
 @mvencode birth_year_opten, mv(99999)
 @egen byear = minimum(birth_year_opten), by(person_id)
-@replace birth_year_opten = byear @if missing(birth_year_opten)
+@replace birth_year_opten = byear @if birth_year_opten == 99999
 @drop byear
 
 # use oldest reported entry date, even if not in data
 # if spell_begin not filled in, use observed year in the data
-@egen first_year_in_market = minimum(minimum(spell_begin, year)), by(person_id)
+@egen first_year_in_market = minimum(minimum([spell_begin, year])), by(person_id)
 
 # flag those that were owners first as "entrepreneurs"
 @replace first_year_as_owner = missing @if first_year_as_owner > year
-@generate entrepreneur = (first_year_as_owner <= minimum(spell_begin, year))
+@generate entrepreneur = (first_year_as_owner <= minimum([spell_begin, year]))
 @drop outsider
-@generate byte outsider = !entrepreneur
+@generate outsider = !entrepreneur
 
 # keep the most senior outsider as the unique ceo
 @generate priority = (first_year_in_market - 1940) + 100*(1-outsider)
-@egen rank = rank(priority), by(frame_id_numeric, year)
+@sort priority
+@egen rank = _n, by(priority, frame_id_numeric, year)
 @keep @if rank==1
 
-@gen aux_cond = year @if year <= 2017
-@egen last_year_before_2017 = maximum(aux_cond), by(frame_id_numeric)
+@generate cond = cond(year <= 2017, year, -99)
+@egen last_year_before_2017 = maximum(cond), by(frame_id_numeric)
 @keep @if (year == last_year_before_2017) | ((frame_id_numeric == 20702230) & (year == 2020))
 # 2017 data for firm is missing from ceo-panel, but can be recovered from the cegjegyzek
 # For me it has to be added completely manually
-@keep first_year_in_market outsider expat birth_year_opten frame_id_numeric year person_id entrepreneur
-@count 
-#set obs `=`r(N)'+1'
-@replace frame_id_numeric = 20702230 @if missing(frame_id_numeric)
+@replace frame_id_numeric = 20702230 @if ismissing(frame_id_numeric)
 @replace year = 2017 @if frame_id_numeric == 20702230 
 @replace first_year_in_market = 2000 @if frame_id_numeric == 20702230 
 @replace outsider = 0 @if frame_id_numeric == 20702230
 @replace expat = 0 @if frame_id_numeric == 20702230
 @replace entrepreneur = 1 @if frame_id_numeric == 20702230
 @replace birth_year_opten = 1956 @if frame_id_numeric == 20702230
+
+@keep first_year_in_market outsider expat birth_year_opten frame_id_numeric year person_id entrepreneur
+@count 
+#set obs `=`r(N)'+1'
 
 @generate in_cegjegyzek = 1
 
